bf0c422ecb33578efbe481e7f2bf8f03b098078e
Tony <economic_luojie@outlook.com>
2024-02-06 16:18:26 +0800

    feat: vue3|基于组合式API进行事件处理

--- vue3-learning/src/views/skill/Method.vue
+++ vue3-learning/src/views/skill/Method.vue
@@ -1,10 +1,19 @@
 <template>
-
+  <button @click="increment">Count is: {{ count }}</button>
 </template>
 
 <script setup lang="ts">
-import { watch, ref ,reactive} from 'vue'
+import { ref, onMounted } from 'vue'
 
+const count = ref(0)
+
+function increment() {
+  count.value++
+}
+
+onMounted(() => {
+  console.log(`The initial count is ${count.value}.`)
+})
 </script>
 
 <style scoped>
--- vue3-learning/src/views/skill/OptionApi.vue
+++ vue3-learning/src/views/skill/OptionApi.vue
@@ -75,7 +75,14 @@ export default{
         },
       ],
       sum2: 1,
-
+      author: {
+        name: 'John Doe',
+        books: [
+          'Vue 2 - Advanced Guide',
+          'Vue 3 - Basic Guide',
+          'Vue 4 - The Mystery'
+        ]
+      }
     }
   },
   computed:{
