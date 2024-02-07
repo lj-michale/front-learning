1cac03a27e56d18b062b98df9b4fdca357be1b7b
Tony <economic_luojie@outlook.com>
2024-02-07 15:17:55 +0800

    feat: vue3|组件嵌套关系理解

--- vue3-learning/src/views/skill/ComponentNestRoot.vue
+++ vue3-learning/src/views/skill/ComponentNestRoot.vue
@@ -1,12 +1,29 @@
 <template>
-
+  <!-- 第三步：显示组件 -->
+  <Header></Header>
+  <Main></Main>
+  <Aside></Aside>
 </template>
 
-<script setup lang="ts">
+<script>
 import {ref,computed, watch, onMounted,reactive} from 'vue';
+//第一步：引入组件
+import Header from '@/views/skill/Header.vue'
+import Main from '@/views/skill/Main.vue'
+import Aside from '@/views/skill/Aside.vue'
 
+//第二步：注入组件
+export default{
+  components:{
+    Header,
+    Main,
+    Aside
+  }
+}
 </script>
 
 <style scoped>
 
 </style>
+
+
