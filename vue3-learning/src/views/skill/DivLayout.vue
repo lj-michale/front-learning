691e58e59cfd87b9fa60b6e69d717820617baa92
Tony <economic_luojie@outlook.com>
2024-02-23 17:13:23 +0800

    feat: div|多列等分布局|浮动 + 百分数平分

--- vue3-learning/src/views/skill/DivLayout.vue
+++ vue3-learning/src/views/skill/DivLayout.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="w">
-    <div class="c">自适应</div>
-    <div class="l">定宽</div>
-    <div class="r">定宽</div>
+    <div class="col col1"></div>
+    <div class="col col2"></div>
+    <div class="col col3"></div>
+    <div class="col col4"></div>
+    <div class="col col5"></div>
   </div>
 </template>
 
@@ -11,34 +13,24 @@
 </script>
 
 <style scoped>
-.w {
-  /* margin-left对应左边元素l的宽度，margin-right对应右边元素r的宽度 */
-  margin-left: 400px;
-  margin-right: 400px;
-}
-.l, .c, .r {
-  height: 600px;
+.col {
   float: left;
+  width: 20%;
+  height: 300px;
 }
-.l {
-  width: 400px;
-  background-color: aqua;
-  position: relative;
-  /* 为了让l元素从当前行移动到第一行同一位置*/
-  margin-left: -100%;
-  /* 移动到中间元素左侧正确位置 */
-  left: -400px;
+.col1 {
+  background-color: blue;
 }
-.c {
-  width: 100%;
+.col2 {
   background-color: blueviolet;
 }
-.r {
-  width: 400px;
-  background-color: brown;
-  position: relative;
-  /* 为了让l元素从当前行移动到上一行*/
-  margin-left: -400px;
-  right: -400px;
+.col3 {
+  background-color: aqua;
+}
+.col4 {
+  background-color: beige;
+}
+.col5 {
+  background-color: salmon;
 }
 </style>
