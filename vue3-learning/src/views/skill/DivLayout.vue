57c43cd449b219616ce6587f8455fbb6f43217fc
Tony <economic_luojie@outlook.com>
2024-02-23 16:34:51 +0800

    feat: vue3|水平垂直居中布局|使用flex进行居中布局

--- vue3-learning/src/views/skill/DivLayout.vue
+++ vue3-learning/src/views/skill/DivLayout.vue
@@ -10,16 +10,14 @@
 
 <style scoped>
 .wrap {
-  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
   width: 1200px;
   height: 800px;
   background-color: aqua;
 }
 .content {
-  position: absolute;
-  left: 50%;
-  top: 50%;
-  transform: translate(-50%, -50%);
   width: 200px;
   height: 200px;
   background-color: blueviolet;
